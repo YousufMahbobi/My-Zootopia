f4df9e0fbdb4a98321a1cbcdc96675621ee48fc5
Sayed Yousuf Mahbobi <sayedyousufmahbobi@Sayeds-MacBook-Air.local>
2025-06-26 15:37:36 +0200

    clean code implemented

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,41 +1,48 @@
 from data.data_service import load_data
 from animals_file_management import read_html_template, write_html_content
 
-
-
 animals_data = load_data()
 
 fields_to_print = {
     "characteristics_diet": lambda x: x['diet']  if isinstance(x, dict) and x.get('diet') else None,
     "locations": lambda x: x[0] if isinstance(x, list) and x else None,
     "characteristics_type": lambda x: x['type'] if isinstance(x, dict) and x.get('type') else None,
-
 }
 
 output = ''
 
-for animal in animals_data:
+
+def serialize_animal(animal_obj):
+    output = ''
     output += '<li class="cards__item">'
-    output += f'<div class="card__title">{animal["name"]}</div>'
+    output += f'<div class="card__title">{animal_obj["name"]}</div>'
     output += '<p class="card__text">'
 
     for field, handler in fields_to_print.items():
         if '_' in field:
             field = field.split('_')
             base_key = field[0]
-            actual_key =  field[1]
+            actual_key = field[1]
         else:
             base_key = field
             actual_key = field
 
-        value = animal.get(base_key)
+        value = animal_obj.get(base_key)
         if value is not None:
             processed = handler(value)
             if processed:
-                    output += f"<strong>{actual_key.title()}:</strong> {processed} <br/>"
+                output += f"<strong>{actual_key.title()}:</strong> {processed} <br/>"
 
     output += '</p>'
     output += '</li>\n'
+
+    return output
+
+for animal in animals_data:
+    output += serialize_animal(animal)
+
+
+
 
 html_template = read_html_template()
 html_template = html_template.replace('__REPLACE_ANIMALS_INFO__', output)
