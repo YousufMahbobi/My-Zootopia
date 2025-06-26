5c535d6de7de20126b2fdf7d585803dc648e1b27
Sayed Yousuf Mahbobi <sayedyousufmahbobi@Sayeds-MacBook-Air.local>
2025-06-26 11:19:18 +0200

    removing unnecessary files

--- helpers/color.py
+++
@@ -1,9 +0,0 @@
-class Color:
-    @staticmethod
-    def red(text): return f'\033[91m{text}\033[0m'
-    @staticmethod
-    def green(text): return f'\033[92m{text}\033[0m'
-    @staticmethod
-    def cyan(text): return f'\033[96m{text}\033[0m'
-    @staticmethod
-    def blue(text): return f'\033[94m{text}\033[0m'
